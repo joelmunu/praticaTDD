8c358f85c13668ae41b9ea56bfc19f1bde476b10
joelmunu <joel.munu@gmail.com>
2022-06-17 12:52:02 +0100

    Test 2 superado

--- Contador.py
+++ Contador.py
@@ -1,5 +1,5 @@
 class Contador:
-    def __init__(self, valorLimite, valorInicial, valorIncremento):
+    def __init__(self, valorLimite = None, valorInicial = 0, valorIncremento = 1):
         self.contador = valorInicial
         self.valorInicial = valorInicial
         self.valorIncremento = valorIncremento
